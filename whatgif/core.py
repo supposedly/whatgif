7c86cfe9ff2ee9feb4e9d9feea88caa83ebd260a
ht <eltrhn@gmail.com>
2019-04-10 09:07:07 -0700

    Flesh out core classes

--- whatgif/core.py
+++ whatgif/core.py
@@ -12,23 +12,24 @@ class GIF(MutableSequence):
         self.header = aux.Header(version)
         self.logical_screen_descriptor = aux.LogicalScreenDescriptor(width, height)
         self.global_color_table = aux.ColorTable()
+        self.images = []
     
     def __getitem__(self, idx):
-        ...
+        return self.images.__getitem__(idx)
     
     def __setitem__(self, idx, value):
-        ...
+        self.images.__setitem__(idx, value)
     
-    def __delitem__(self, idx, value):
-        ...
+    def __delitem__(self, idx):
+        self.images.__delitem__(idx)
     
     def __len__(self):
-        ...
+        return self.images.__len__()
     
     def insert(self, idx, value):
-        ...
-
+        self.images.insert(idx, value)
 
 
 class Frame:
-    def __init__(self, ndarray
+    def __init__(self, ndarray):
+        ...
