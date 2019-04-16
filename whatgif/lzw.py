78cb55a9cb2b8d9aeb41a1ec8c9a82df6f979de1
ht <eltrhn@gmail.com>
2019-04-15 23:33:35 -0700

    Implement BitStream/finish setup for LZW compression

--- whatgif/lzw.py
+++ whatgif/lzw.py
@@ -1,14 +1,31 @@
-import struct
+from functools import reduce
+from itertools import repeat
 
 from . import misc
 
 
-class _BitStream:
+class BitStream:
     def __init__(self):
         self._buffer = []
+        self._out = bytearray()
     
-    def append(self, bits):
-        ...
+    def __bytes__(self):
+        if self._buffer:
+            self._consume_byte()
+        return bytes(self._out)
+    
+    def _consume_byte(self):
+        self._out.append(reduce(lambda acc, bit: (acc << 1) | bit, self._buffer[7::-1]))
+        del self._buffer[:8]
+    
+    def append(self, n, code_size):
+        fill = code_size - n.bit_length()
+        while n:
+            self._buffer.append(n % 2)
+            n //= 2
+        self._buffer.extend(repeat(0, fill))
+        if len(self._buffer) >= 8:
+            self._consume_byte()
 
 
 class CodeTable:
@@ -17,13 +34,13 @@ class CodeTable:
     def __init__(self, color_table):
         self._d = {}
         self.min_code_size = max(self.MAX_CODE_SIZE, 1 + color_table.size())
-        self._code_size = 1 + self.min_code_size  # initialize to 'first code size'
+        self._code_size = self._first_code_size = 1 + self.min_code_size
         self._max_code = 2 ** self.min_code_size - 1
         self.clear = 1 + self._max_code
         self.eoi = 1 + self.clear
         self._cur_code = 0
         self._codes_used = set()
-        self.out = _BitStream()
+        self.out = BitStream()
     
     def __contains__(self, key):
         return self._d.__contains__(key)
@@ -42,11 +59,11 @@ class CodeTable:
     
     def output(self, indices):
         code = self[indices]
-        self.out.append(code)
+        self.out.append(code, self._code_size)
         if code.bit_length() == self._code_size:
             if self._code_size == self.MAX_CODE_SIZE:
-                self.out.append(self.clear)
-                self._code_size = 1 + self.min_code_size  # reset to 'first code size'
+                self.out.append(self.clear, self._code_size)
+                self._code_size = self._first_code_size
             else:
                 self._code_size += 1
 
@@ -65,4 +82,4 @@ def compress(data, color_table, color_indices):
         idx_buffer = k,
     code_table.output(idx_buffer)
     code_table.output(code_table.eoi)
-    return code_table.out
+    return bytes(code_table.out)
