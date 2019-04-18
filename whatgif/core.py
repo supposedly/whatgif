479ee6f1d490a134f5ec98d9473ef1696e4b6d57
ht <eltrhn@gmail.com>
2019-04-17 23:05:10 -0700

    Flesh out core.Frame

--- whatgif/core.py
+++ whatgif/core.py
@@ -36,20 +36,29 @@ class GIF(MutableSequence):
 
 
 class Frame:
-    def __init__(self, pixels, gif):
+    def __init__(self, pixels, gif, color_table=None):
         if not isinstance(pixels, np.ndarray):
             pixels = np.array(pixels)
+        if color_table is None:
+            color_table = gif.global_color_table
+        self.color_table = color_table
         self.image_descriptor = classes.ImageDescriptor(*pixels.shape[:2])
-        self.data = pixels
-        self.colors = set(self.data.flat)
+        self.data = np.vectorize(self.color_table.__getitem__)(pixels)
+        self.colors = set(pixels.flat)
         self.gif = gif
+    
+    def __imod__(self, other):
+        if not isinstance(other, Frame):
+            return NotImplemented
+        self.data[self == other] = 0
+        return self
     
     def __bytes__(self):
         ...
     
     def update_color_table(self):
-        self.gif.global_color_table.extend(
-          self.colors.difference(self.gif.global_color_table)
+        self.color_table.extend(
+          self.colors.difference(self.color_table)
         )
     
     def compress(self):
